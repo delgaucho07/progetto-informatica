17e260c33b79222b912730b6e7e5ce587f76f47b
delgaucho07 <delgaudio.riccardo.21@itisriva.edu.it>
2026-04-28 13:12:41 +0200

    Add files via upload

---
+++ maneggio/Migrations/20260428102424_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace maneggio.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Utenti",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ruolo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utenti", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prenotazioni",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UtenteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prenotazioni", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Prenotazioni_Utenti_UtenteId",
+                        column: x => x.UtenteId,
+                        principalTable: "Utenti",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prenotazioni_UtenteId",
+                table: "Prenotazioni",
+                column: "UtenteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prenotazioni");
+
+            migrationBuilder.DropTable(
+                name: "Utenti");
+        }
+    }
+}
